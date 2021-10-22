3fc7421af0daba6bdb53e49f160e09223d921a01
thang19411 <thangnq19411@st.uel.edu.vn>
2021-10-22 16:43:02 +0700

    vd

--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //sdfasffasdf
         //csvdvzc
+        //fsvdf
+        //fdvdvfbg ngng
     }
 }
