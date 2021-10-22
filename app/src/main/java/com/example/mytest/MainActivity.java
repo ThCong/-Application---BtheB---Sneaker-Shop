239607eefe308fc3991f04c46747622f5b3e8f15
ThanhTruc1810 <thanhtruc2016qnm@gmail.com>
2021-10-22 22:53:48 +0700

    addButton

--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -1,18 +1,8 @@
 package com.example.mytest;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-
-
-import com.example.mytest.ui.home.HomeFragment;
-import com.example.mytest.ui.more.MoreFragment;
-import com.example.mytest.ui.notifications.NotificationsFragment;
-import com.example.mytest.ui.search.SearchFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,33 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //sdfasffasdf
+        //csvdvzc
+        //ấdfsdfsdfsadfsadf
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemReselectedListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
     }
-    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemReselectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment selectedFragment = null;
-            switch (item.getItemId())
-            {
-                case R.id.navigation_home:
-                    selectedFragment = new HomeFragment();
-                    break;
-                case R.id.navigation_search:
-                    selectedFragment = new SearchFragment();
-                    break;
-                case R.id.navigation_notifications:
-                    selectedFragment = new NotificationsFragment();
-                    break;
-                case R.id.navigation_more:
-                    selectedFragment = new MoreFragment();
-                    break;
-            }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
-
-            return true;
-        }
-    };
 }
