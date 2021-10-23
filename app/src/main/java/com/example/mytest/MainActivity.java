9a439fad1810e907a3041c5c15ea71be4744b2ea
ThCong <91537767+ThCong@users.noreply.github.com>
2021-10-23 13:17:25 +0700

    Bannerslider

--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.mytest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.mytest.ImageApapter.ImageAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    ImageAdapter imageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //sdfasffasdf
+        linkViews();
+        loadData();
+    }
+
+    private void linkViews() {
+        viewPager = findViewById(R.id.viewPager);
+    }
+    private void loadData() {
+        imageAdapter = new ImageAdapter(this);
+        viewPager.setAdapter(imageAdapter);
     }
 }
