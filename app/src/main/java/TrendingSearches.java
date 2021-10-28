dbf38d4f634b66e4a8586ddc31c90bc308dad089
ThCong <91537767+ThCong@users.noreply.github.com>
2021-10-28 20:56:26 +0700

    Squashed commit of the following:
    commit 3a345751aaef8a6ce023cfd163af4ae17ac7f1e6
    Author: uyendtm <uyendtm19411@st.uel.edu.vn>
    Date:   Thu Oct 28 20:30:59 2021 +0700
    
        BrandList and SearchByKey Layout
    
        Danh sách Brands và giao diện tìm kiếm theo từ khóa
    
    commit ccce50db90d14c321d6450e951302836dc687f6e
    Author: uyendtm <uyendtm19411@st.uel.edu.vn>
    Date:   Sat Oct 23 22:54:56 2021 +0700
    
        lvBrand
    
    commit 229220d4558a97892b0659f90866eb9179c3448b
    Merge: 707c8d0 1c0376a
    Author: ThCong <91537767+ThCong@users.noreply.github.com>
    Date:   Fri Oct 22 22:46:57 2021 +0700
    
        Merge pull request #2 from ThCong/cong1
    
        Cong1
    
    commit 707c8d069e30394489e68f53f64bcd070056a077
    Merge: 3a44a6f 2d63d4b
    Author: ThCong <91537767+ThCong@users.noreply.github.com>
    Date:   Fri Oct 22 14:57:01 2021 +0700
    
        Merge pull request #1 from ThCong/cong1
    
        Comment

---
+++ app/src/main/java/TrendingSearches.java
@@ -0,0 +1,15 @@
+public class TrendingSearches {
+    String name;
+
+    public TrendingSearches(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
