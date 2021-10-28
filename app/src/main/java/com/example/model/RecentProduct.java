3a345751aaef8a6ce023cfd163af4ae17ac7f1e6
uyendtm <uyendtm19411@st.uel.edu.vn>
2021-10-28 20:30:59 +0700

    BrandList and SearchByKey Layout
    Danh sách Brands và giao diện tìm kiếm theo từ khóa

---
+++ app/src/main/java/com/example/model/RecentProduct.java
@@ -0,0 +1,20 @@
+package com.example.model;
+
+public class RecentProduct {
+    private String Recent;
+
+    public RecentProduct(String recent) {
+        Recent = recent;
+    }
+
+    public String getRecent() {
+        return Recent;
+    }
+
+    public void setRecent(String recent) {
+        Recent = recent;
+    }
+
+    public RecentProduct() {
+    }
+}
---
+++ app/src/main/java/com/example/model/TrendingProduct.java
@@ -0,0 +1,21 @@
+package com.example.model;
+
+public class TrendingProduct {
+
+    private String Trending;
+
+    public TrendingProduct(String trending) {
+        Trending = trending;
+    }
+
+    public String getTrending() {
+        return Trending;
+    }
+
+    public void setTrending(String trending) {
+        Trending = trending;
+    }
+
+    public TrendingProduct() {
+    }
+}
