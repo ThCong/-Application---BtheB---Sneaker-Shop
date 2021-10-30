1696f1bf29a9ecec9a8a3ed9e976fbb205c629ed
ThanhTruc1810 <thanhtruc2016qnm@gmail.com>
2021-10-31 00:01:37 +0700

    more

---
+++ app/src/main/java/com/example/model/Product.java
@@ -0,0 +1,21 @@
+package com.example.model;
+
+public class Product {
+    private int productThumb;
+
+    public int getProductThumb() {
+        return productThumb;
+    }
+
+    public void setProductThumb(int productThumb) {
+        this.productThumb = productThumb;
+    }
+
+
+
+
+    public Product(int productThumb) {
+        this.productThumb = productThumb;
+
+    }
+}
--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.mytest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.example.adapter.ProductAdapter;
+import com.example.model.Product;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rcvProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +25,38 @@ public class MainActivity extends AppCompatActivity {
         //sdfasffasdf
         //csvdvzc
         //ấdfsdfsdfsadfsadf
+
+        LinkViews();
+        confiRecycleView();
+        initData();
+    }
+
+    private void LinkViews() {
+        rcvProduct = findViewById(R.id.rcvProduct);
+    }
+
+    private void confiRecycleView() {
+        LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        rcvProduct.setLayoutManager(manager);
+
+
+
+        rcvProduct.setHasFixedSize(true);
+        rcvProduct.setItemAnimator(new DefaultItemAnimator());
+    }
+
+    private void initData() {
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product(R.drawable.shoes2));
+        products.add(new Product(R.drawable.shoes2));
+        products.add(new Product(R.drawable.shoes2));
+        products.add(new Product(R.drawable.shoes2));
+        products.add(new Product(R.drawable.shoes2));
+
+
+        ProductAdapter adapter = new ProductAdapter(getApplicationContext(), products);
+        rcvProduct.setAdapter(adapter);
+
+
     }
 }
