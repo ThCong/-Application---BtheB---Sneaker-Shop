07b4d5be5fcb2adf0c9c42e23d5bb1643a101dcc
ThCong <91537767+ThCong@users.noreply.github.com>
2021-11-01 00:38:07 +0700

    linhtin

--- app/src/main/java/com/example/mytest/Signin.java
+++ app/src/main/java/com/example/mytest/Signin.java
@@ -19,90 +19,11 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Signin extends AppCompatActivity {
 
-    EditText edtEmail, edtPassword;
-    TextView tvForgotPassword;
-    Button btnSignIn, btnSignUp;
-    boolean isEmailvalid, isPasswordvalid;
-    TextInputLayout emailError, passError;
-    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signin_layout);
-
-        linkViews();
-        AddEvents();
-
-    }
-    private void linkViews() {
-        edtEmail = findViewById(R.id.edtEmail);
-        edtPassword = findViewById(R.id.edtPassword);
-        tvForgotPassword = findViewById(R.id.tvForgotPassword);
-        btnSignIn = findViewById(R.id.btnSignIn);
-        btnSignUp = findViewById(R.id.btnSignUp);
-        fAuth = FirebaseAuth.getInstance();
-    }
-
-    private void AddEvents() {
-
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (edtEmail.getText().length() > 0 && edtPassword.getText().length() > 0) {
-                    String toastMessage = "Welcome " + edtEmail.getText().toString();
-                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
-                } else {
-                    String toastMessage = "Username or Password is False";
-                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
-
-        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText resetMail = new EditText(v.getContext());
-                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
-                passwordResetDialog.setTitle("Reset Password?");
-                passwordResetDialog.setMessage("Enter your email to receive reset password link.");
-                passwordResetDialog.setView(resetMail);
-
-                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //extract the email and send reset link
-                        String mail = resetMail.getText().toString();
-                        fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                Toast.makeText(Signin.this, "Reset link sent to your email", Toast.LENGTH_SHORT).show();
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(Signin.this, "Error! Reset link is not sent to your email" + e.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-
-                    }
-                });
-                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //close dialog
-                    }
-                });
-
-                passwordResetDialog.create().show();
-            }
-        });
-
-
-
-    }
+        setContentView(R.layout.signin_layout);    }
 
 
 }
