cebb6eabff1a3332362291e7f0dbc56d31f8e8ed
ThCong <91537767+ThCong@users.noreply.github.com>
2021-11-01 01:41:04 +0700

    Merge_Thu

---
+++ app/src/main/java/com/example/mytest/Forgot_pwd.java
@@ -0,0 +1,14 @@
+package com.example.mytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forgot_pwd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pwd);
+    }
+}
